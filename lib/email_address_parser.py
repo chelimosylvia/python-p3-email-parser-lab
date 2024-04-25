9e59cf7e7f25d25cf919cbc5043751356ba06e4d
Sylviachelimo <chelimosylvia280@gmail.com>
2024-04-25 10:32:31 +0300

    Completed

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
 # your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        parts = re.split(r'[,\s]+', self.email_addresses)
+        parsed_emails = []
+        for part in parts:
+            if '@' in part:
+                parsed_emails.append(part)
+        return sorted(parsed_emails)
